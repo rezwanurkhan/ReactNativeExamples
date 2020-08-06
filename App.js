ceccbc6a657edda200fbebf796212ce1a7e32faf
rezwanur.khan <rezwanur.khan@bs-23.net>
2020-08-06 12:30:32 +0600

    pagination added

--- App.js
+++ App.js
@@ -1,50 +1,89 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  FlatList,
+  View,
+  Text,
+  ActivityIndicator,
+  SafeAreaView,
+} from 'react-native';
 
-export default App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState({
-    isLoading: true,
-    items: [],
-    page: 0,
-  });
-  const itemPerPage = 15;
+const App = () => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [isLoading, setLoading] = useState(false);
+  const itemPerPage = 30;
 
   useEffect(() => {
-    fetchData(0);
-  });
+    setLoading(true);
+    const apiURL = `https://api.instantwebtools.net/v1/passenger?page=${page}&size=${itemPerPage}`;
 
-  const fetchData = (page) => {
-    return fetch(
-      `https://api.instantwebtools.net/v1/passenger?page=${page}&size=${itemPerPage}`,
-    )
-      .then((response) => response.json())
-      .then((json) =>
-        setData({
-          isLoading: false,
-          items: [],
-          page: data.page++,
-        }),
-      )
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((resJson) => {
+        setData(data.concat(resJson.data));
+        setLoading(false);
+      });
+
+    return () => {};
+  }, [page]);
+
+  const renderRow = ({index, item}) => {
+    return (
+      <View style={styles.itemRow}>
+        <Text style={styles.itemText}>{`${index}) ` + item.name}</Text>
+      </View>
+    );
+  };
+
+  const renderFooter = () => {
+    // ActivityIndicator Displays a circular loading indicator.
+    return isLoading ? (
+      <View style={styles.loader}>
+        <ActivityIndicator color="red" size="large" />
+      </View>
+    ) : null;
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
   };
 
   return (
-    <View style={{flex: 1, padding: 24}}>
-      {isLoading ? (
-        <ActivityIndicator color="red" size="large" />
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({_id}, index) => _id}
-          renderItem={({item}) => (
-            <Text>
-              {item.name}, {item.trips}
-            </Text>
-          )}
-        />
-      )}
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <FlatList
+        style={styles.container}
+        data={data}
+        renderItem={renderRow}
+        keyExtractor={(item, index) => index.toString()}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={renderFooter}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    marginTop: 5,
+    backgroundColor: '#f5fcff',
+  },
+  itemRow: {
+    backgroundColor: '#e1e1e1',
+    marginVertical: 5,
+    padding: 10,
+    borderRadius: 10,
+  },
+  itemText: {
+    fontSize: 16,
+    padding: 5,
+  },
+  loader: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+});
+
+export default App;
